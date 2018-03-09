25f26a7a356ea9246f325361be228272eb0352b9
Gergana Mileva <ravenmorte10@gmail.com>
2018-03-09 14:12:47 +0200

    Documented solution of Problem 2

--- intro-and-basic-syntax/exercise/src/MainClass.java
+++ intro-and-basic-syntax/exercise/src/MainClass.java
@@ -2,10 +2,15 @@ import java.util.Scanner;
 
 public class MainClass {
     public static void main(String[] args) {
+        // We add scanner util so we can read input
         Scanner scanner=new Scanner(System.in);
+        // we read our input -> side a
         double a=scanner.nextDouble();
+        // we read our input -> side b
         double b=scanner.nextDouble();
+        // we calculate the area using the formula area = a * b
         double area=a*b;
+        // we print the result to the second digit after the decimal point
         System.out.printf("%.2f", area);
     }
 }
