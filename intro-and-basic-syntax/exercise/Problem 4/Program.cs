e1ddecc44dc5788ce38af619abc6e8fb5e6562c0
Milena Aleksieva <milenaaleksieva@abv.bg>
2018-03-10 15:04:01 +0200

    Changed variable name in exercise/Problem 4/Program.cs

--- intro-and-basic-syntax/exercise/Problem 4/Program.cs
+++ intro-and-basic-syntax/exercise/Problem 4/Program.cs
@@ -7,20 +7,20 @@ namespace BeverageLabels
         static void Main(string[] args)
         {
             // Console info for name of product:
-            string name = Console.ReadLine();
+            string productName = Console.ReadLine();
 
             // Console info for volume of product:
-            int volume = int.Parse(Console.ReadLine());
+            int volumeOfProduct = int.Parse(Console.ReadLine());
 
             //Console info for energy of product:
-            int energy = int.Parse(Console.ReadLine());
+            int energy= int.Parse(Console.ReadLine());
 
             //Console info for sugar content of product:
             int sugar = int.Parse(Console.ReadLine());
 
             //Calculation of energy and sugar content for given volume:
-            double enContent = (volume * energy * 1.0) / 100;
-            double sugContent = (volume * sugar * 1.0) / 100;
+            double energyContent = (volume * energy * 1.0) / 100;
+            double sugarContent = (volume * sugar * 1.0) / 100;
 
             //Print:
             Console.WriteLine($"{volume}ml {name}:");
