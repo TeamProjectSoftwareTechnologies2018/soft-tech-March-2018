a041ee05a21501266e3d4213841b7138d710ee2c
Gergana Mileva <ravenmorte10@gmail.com>
2018-03-11 10:28:36 +0200

    Documented Solution to Problem 3

--- intro-and-basic-syntax/exercise/Problem 3/Program.cs
+++ intro-and-basic-syntax/exercise/Problem 3/Program.cs
@@ -1,14 +1,16 @@
 ﻿using System;
-
+// Confirmed by  Gergana Mileva (allsi89)
 namespace MilesToKilometers
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Receive and read a floating point number from the console
             double miles = double.Parse(Console.ReadLine());
+            // Convert miles to kilometers with the formula down below
             double km = miles * 1.60934;
-
+            // Print the result -> a number to the second digit after the floating point
             Console.WriteLine($"{km:f2}");
         }
     }
